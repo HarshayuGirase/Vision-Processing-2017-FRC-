d2b51df5ccc82b4a79c384cef907123fc22719c9
Harshayu Girase <harhsayu7@gmail.com>
2017-02-04 15:03:31 -0800

    bruhhh

--- Server.py
+++ Server.py
@@ -5,8 +5,11 @@ TCP_PORT = 2721
 BUFFER_SIZE = 256
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates the socket
+print 'socket created'
 s.bind((TCP_IP, TCP_PORT)) #binds the socket to certain address using IP and Port#
+print 'socket bind'
 s.listen(1) #look out for 1 connection
+print 'socket listen'
 
 conn, addr = s.accept()
 
