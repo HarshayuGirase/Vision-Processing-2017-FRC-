df3049849538a1bc0e517f23c4285ab5ea58b9a4
Harshayu Girase <harhsayu7@gmail.com>
2017-02-04 22:04:38 -0800

    cool

--- Server.py
+++ Server.py
@@ -4,7 +4,6 @@ TCP_IP = socket.gethostname()
 TCP_PORT = 2773
 BUFFER_SIZE = 256
 print TCP_IP 
-print 'lol'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates the socket
 print 'socket created'
