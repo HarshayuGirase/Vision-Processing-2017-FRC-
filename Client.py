c3b92f3ae2b4796a049fd86c2aed4b9bf5ca6926
Harshayu Girase <harhsayu7@gmail.com>
2017-02-06 13:45:57 -0800

    commit

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 import socket
 
-TCP_IP = '192.168.1.12'
-TCP_PORT = 2776
+TCP_IP = '127.0.0.1'
+TCP_PORT = 2779
 BUFFER_SIZE = 256
 REQUESTEDMESSAGE = 'Is Nikhil hot?'
 
--- Server.py
+++ Server.py
@@ -1,13 +1,14 @@
 import socket
 
-TCP_IP = socket.gethostname()
-TCP_PORT = 2776
+TCP_IP = '127.0.0.1'
+TCP_PORT = 2779
 BUFFER_SIZE = 256
 print TCP_IP 
 print 'lol'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creates the socket
 print 'socket created'
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #so it can be recreated
 s.bind((TCP_IP, TCP_PORT)) #binds the socket to certain address using IP and Port#
 print 'socket bind'
 s.listen(1) #look out for 1 connection
