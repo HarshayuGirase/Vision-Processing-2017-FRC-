b775d99a1d401c37ad4988ef0311a02362645cf2
Harshayu Girase <harhsayu7@gmail.com>
2017-02-13 15:43:25 -0800

    trying to make it work both ways

--- UDPClient.py
+++ UDPClient.py
@@ -10,16 +10,17 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 s.bind((host, port))
 
-start_time = time.time()
-while (time.time() - start_time < 10):
+connectedToServer = False
+while (connectedToServer == False):
     try:
         message, address = s.recvfrom(8192)
         if(message=='SFHS Server FUCK254'):
         	SERVER_IP = address[0]
         	print 'Got SFHS Server IP Address'
+        	connectedToServer = True
     except (KeyboardInterrupt, SystemExit):
         raise
     except:
         traceback.print_exc()
 
-print SERVER_IP
+print 'I now have the SFHS Server IP: ' + SERVER_IP
