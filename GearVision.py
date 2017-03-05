ebd12fe45b7abd6babc2ba5cdf43b40eda6df075
Harshayu Girase <harhsayu7@gmail.com>
2017-03-04 18:22:28 -0800

    updated shit... hella

---
+++ GearVision.py
@@ -0,0 +1,94 @@
+import numpy as np
+import cv2
+import time
+import multiprocessing
+import math
+
+
+
+img = cv2.imread('/Users/harshayugirase/Desktop/LiveFeed/image0.bmp', cv2.IMREAD_UNCHANGED)
+img = cv2.cvtColor(np.uint16(img),cv2.COLOR_GRAY2RGB) 
+
+def getDistanceAngle(xCoordinate):
+	CENTERX = 320
+	angle = (xCoordinate - 320)/10
+	return angle
+
+def lowGoalCovered():
+	kernel = np.ones((3,3))
+	ret,threshold = cv2.threshold(np.uint8(img),0,60000,cv2.THRESH_BINARY)
+	erosion = cv2.erode(threshold,kernel,iterations = 5) #increase if necessary 
+	dilation = cv2.dilate(erosion,kernel,iterations = 5)
+	start_time = time.clock()
+	edges = cv2.Canny(np.uint8(dilation),4,2) #edge detection after some noise filtering   
+
+	#cv2 version returns 2 or 3 depending on version :/
+	try:
+		contours,_ = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE) #finds all contours
+		contoursParent, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #finds parent contours
+	except:
+		_,contours,_ = cv2.findContours(edges.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #finds all contours
+		_,contoursParent, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #finds parent contours
+
+
+	xCenterValues = []; yCenterValues = []
+	FINALCONTOURS = []
+
+	for i in range(0, len(contoursParent)): #essentially only keeps the children contours
+		for x in range(0, len(contours)):
+			same = True
+			try:
+				if(len(contoursParent[i])==len(contours[x])):
+					for check in range(0,len(contoursParent[i])):
+						if(contoursParent[i][check][0][0]==contours[x][check][0][0] and contoursParent[i][check][0][1]==contours[x][check][0][1]):
+							lolz = 0 #nothing lol
+						else:
+							same = False
+				else:
+					same = False
+				break;
+			except ValueError:
+				print 'rip'
+
+			if(same==True):
+				contours.pop(x)
+				x=x-1
+
+
+	for c in contours: # compute the center of the contour
+		M = cv2.moments(c)
+		cX = 0
+		cY = 0
+		if(M["m00"]!=0):
+			cX = int(M["m10"] / M["m00"])
+			cY = int(M["m01"] / M["m00"])
+
+		#try to find contour area of low goal and limit size of xCenterValues and yCenterValues, so that only that range will be viewed...
+		width = len(edges[0])
+		height = sum([len(arr) for arr in edges])/width
+
+		if(cY>height/1.75):
+			xCenterValues.append(cX)
+			yCenterValues.append(cY)
+			FINALCONTOURS.append(c)
+
+	for i in range(0,len(yCenterValues)):
+		width = len(edges[0])
+		height = sum([len(arr) for arr in edges])/width
+		if(yCenterValues[i]>0 and yCenterValues[i]<500 and cv2.contourArea(FINALCONTOURS[i])>300 and cv2.contourArea(FINALCONTOURS[i])<900): #modify contour area later
+			cv2.circle(img,(xCenterValues[i],yCenterValues[i]),7,(239,95,255),-1) #draw the circle where center is
+			print ('X Center is: ' + str(xCenterValues[i])) #calculate the x value of the center...
+			print ('Y Center is: ' + str(yCenterValues[i])) #calculate the y value of the center...
+			print ('Contour Area is: ' + str(cv2.contourArea(FINALCONTOURS[i]))) 
+			print ('Angle to goal: ' + str(getDistanceAngle(xCenterValues[i])))
+			cv2.drawContours(img,FINALCONTOURS,i,(0,255,0),3) #draw the contour
+
+	#print("Y Center is: " + str(sum(yCenterValues)/len(yCenterValues)))
+	#print("X Center is: " + str(sum(xCenterValues)/len(xCenterValues)))
+	print("--- %s seconds ---" % (time.clock() - start_time))
+
+	cv2.imwrite('/Users/harshayugirase/Desktop/output.bmp', img)
+	cv2.imwrite('/Users/harshayugirase/Desktop/cannyimage.bmp', edges)
+
+	
+lowGoalCovered()
--- TCPClient.py
+++ TCPClient.py
@@ -5,12 +5,16 @@ import sys
 import time
 import select
 
-IMAGEARRAY = []
+
+RUNTIME = 5 #seconds of how long program should run
+
+COLORIMAGEARRAY = []
+DEPTHIMAGEARRAY = []
 start_time = time.clock()
 print 'Program has started.'
 
-TCP_IP = '192.168.1.37'
-TCP_PORT = 2371
+TCP_IP = '192.168.1.20'
+TCP_PORT = 2373
 BUFFER_SIZE = 9999
 
 print 'Trying to connect...'
@@ -22,33 +26,56 @@ s.setblocking(0)
 
 startframetime = time.time()
 count = 0
-while(time.time() - startframetime < 10):
-	s.send('Gimme latest image')
-	count = count+1
-	imageasstring = ''
-
-	while(len(imageasstring)<614400 and time.time()-startframetime<10):
+while(time.time() - startframetime < RUNTIME):
+	colorimagestring = ''
+	while(len(colorimagestring)<307200 and time.time() - startframetime < RUNTIME):
 		ready = select.select([s], [], [], 0.01)
 		if ready[0]:
 			data = s.recv(BUFFER_SIZE)
-			imageasstring = imageasstring + data
+			colorimagestring = colorimagestring + data
 
-	print 'received image'
+	print 'received color image'
+	print len(colorimagestring)
 	try:
-		if len(imageasstring)==614400:
-			backtoarray = np.fromstring(imageasstring, np.int16).reshape(480, 640)
-			IMAGEARRAY.append(backtoarray)
+		if len(colorimagestring)==307200:
+			backtoarray = np.fromstring(colorimagestring, dtype=np.uint8).reshape(480,640)
+			COLORIMAGEARRAY.append(backtoarray)
 			print count
 		else:
 			print 'fucked up'
 	except:
 		print 'fucked up2'
+
+
+	depthimagestring = ''
+	while(len(depthimagestring)<614400 and time.time() - startframetime < RUNTIME):
+		ready = select.select([s], [], [], 0.01)
+		if ready[0]:
+			data = s.recv(BUFFER_SIZE)
+			depthimagestring = depthimagestring + data
+
+	print 'received depth image'
+	print len(depthimagestring)
+	try:
+		if len(depthimagestring)==614400:
+			backtoarray = np.fromstring(depthimagestring, dtype=np.uint16).reshape(480,640)
+			DEPTHIMAGEARRAY.append(backtoarray)
+			print count
+		else:
+			print 'fucked up'
+	except:
+		print 'fucked up2'
+
 	
+
 s.close()
 
 print (time.clock() - start_time)
 
-for i in range(0,len(IMAGEARRAY)):
-	cv2.imwrite('/Users/harshayugirase/Desktop/LiveFeed/image' + str(i) + '.png', IMAGEARRAY[i])
+for i in range(0,len(COLORIMAGEARRAY)):
+	cv2.imwrite('/Users/harshayugirase/Desktop/LiveFeed/image' + str(i) + '.bmp', COLORIMAGEARRAY[i])
+
+for i in range(0,len(DEPTHIMAGEARRAY)):
+	cv2.imwrite('/Users/harshayugirase/Desktop/LiveFeed/image' + str(i) + '.png', DEPTHIMAGEARRAY[i])
 
 print 'Program done running :D'
